9d965f20d71f6fe5072ccb38a8259d66728d7211
Marvin Siri <marvinsiri123@gmail.com>
2023-12-04 15:09:06 -0500

    db working

---
+++ src/db/models/Pets.js
@@ -0,0 +1,19 @@
+const knex = require('./knex');
+
+class Pets {
+  // This is just an example query, obviously you need to change it, but it shows you how to use knex.raw and dynamic values
+  static async create(data) {
+    try {
+      const query = `INSERT INTO pets (data) values (?) returning *`;
+      const {
+        rows: [newToDo],
+      } = await knex.raw(query, [data]);
+      return newToDo;
+    } catch (err) {
+      console.error(err);
+      return null;
+    }
+  }
+}
+
+module.exports = Pets;
